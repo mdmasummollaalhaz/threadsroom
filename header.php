be4ceb5392bd393f1e470e6a6dcd8ac6a2185bf6
mdmasummollaalhaz <freelancermasummolla@gmail.com>
2022-04-02 22:35:31 +0600

    Mobile Nav dynamac

--- header.php
+++ header.php
@@ -49,12 +49,12 @@
 			 <nav id="site-navigation" class="main-navigation">
 				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'garydavis' ); ?></button>
 				<?php
-				wp_nav_menu(
-					array(
-						'theme_location' => 'menu-1',
-						'menu_id'        => 'primary-menu',
-					)
-				);
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+						)
+					);
 				?>
 			</nav> <!-- #site-navigation -->
 		</div>
@@ -72,10 +72,18 @@
 						</div>
 						<div class="overlay">
 							<div class="overlayContent">
-								<a href="#" id="services">Services</a>
+								<?php
+									wp_nav_menu(
+										array(
+											'theme_location' => 'menu-1',
+											'menu_id'        => 'primary-menu',
+										)
+									);
+									?>
+								<!-- <a href="#" id="services">Services</a>
 								<a href="#" id="work">Our work</a>
 								<a href="#" id="contact">Contact</a>
-								<p>Copyright 2018</p>
+								<p>Copyright Threadsroom 2022</p> -->
 							</div>
 						</div>
 					</div>
